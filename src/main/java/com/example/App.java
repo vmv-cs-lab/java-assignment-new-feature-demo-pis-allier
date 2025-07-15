fe6643a381b73b564f7155554c809b9701717199
jagat <jagat.vmv@gmail.com>
2025-07-15 10:19:55 +0530

    submit assignment java starter demo

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,7 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, student!");
+        System.out.println("result of method "+App.add);
     }
 
     // Example method students will complete or modify
